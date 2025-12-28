b0a18bbc2a5928ce24d206f468c7001d78a31501
Joy <kaosarjoy52@gmail.com>
2025-12-28 17:29:30 +0600

    database update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = 3000
 // Middleware
@@ -35,11 +35,11 @@ async function run() {
     const activitiesCollection = db.collection("activities");
 
 //single user insert
-    app.post('/users',async(req,res)=>{
-      const user = req.body;
-      const result = await usersCollection.insertOne(user);
-      res.send(result);
-    })
+    // app.post('/users',async(req,res)=>{
+    //   const user = req.body;
+    //   const result = await usersCollection.insertOne(user);
+    //   res.send(result);
+    // })
 
     //multiple user 
     app.post('/users',async(req,res)=>{
@@ -54,14 +54,57 @@ async function run() {
       res.send(result)
     })
 
-    //activities
+    // find the users
+     app.get('/users/:id',async(req,res)=>{
+      const id = req.params.id
+      const query = {_id : new ObjectId(id)}
+      const result = await usersCollection.findOne(query)
+      res.send(result)
+    }) 
+
+    //users petch
+    app.patch('/users/:id',async(req,res)=>{
+      const id = req.params.id;
+      const updateUser = req.body;
+      const query ={_id : new ObjectId(id)}
+      const update = {
+        $set:{
+          name : updateUser.name
+          
+        }
+      }
+      const result = await usersCollection.updateOne(query,update)
+      res.send(result)
+    })
+
+    //activities data add multipule 
+     app.post('/activities',async(req,res)=>{
+      const activitie = req.body
+      const result = await activitiesCollection.insertMany(activitie)
+    })
+
+    //find the activities 
+       app.get('/activities/:id',async(req,res)=>{
+      const id = req.params.id
+      const query = {_id : new ObjectId(id)}
+      const result = await activitiesCollection.findOne(query)
+      res.send(result)
+    }) 
+
+    //delete activities
+     app.delete('/activities/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result =await activitiesCollection.deleteOne(query)
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
