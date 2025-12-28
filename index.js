448c143167ca506911afac65d8c7ed0fcb57cb7a
Joy <kaosarjoy52@gmail.com>
2025-12-28 15:39:35 +0600

    create the repo

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express')
-const cros = require('cros')
-const app = express()
-const port =process.env.PORT || 3000
+const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
-//middleware
-app.use(cros())
+const app = express()
+const port = 3000
+// Middleware
+app.use(cors())
 app.use(express.json())
+// Routes
+const uri = "mongodb+srv://ecoTrack_UserInfo:Ck4tTq2GZ428abNi@cluster0.uoqvvub.mongodb.net/?appName=Cluster0";
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-const uri = "mongodb+srv://ecoTrack_UserInfo:ecoTrack@cluster0.uoqvvub.mongodb.net/?appName=Cluster0";
-
-//create a client
+// MongoDB Client Setup
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -18,25 +21,20 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
-//mongodb test
+// Connect to MongoDB
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("You successfully connected to MongoDB!");
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-   // await client.close();
+    await client.close();
   }
 }
-run().catch();
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+run().catch(console.dir);
