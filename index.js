905e3823e381393404868faaac5133aacba6cbd2
Joy <kaosarjoy52@gmail.com>
2025-12-28 15:50:14 +0600

    users section create done

--- index.js
+++ index.js
@@ -27,11 +27,38 @@ app.listen(port, () => {
 // Connect to MongoDB
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+
     await client.connect();
+    //create  a database called 
+    const db = client.db("ecoTrackDB");
+    const usersCollection = db.collection("users");
+    const activitiesCollection = db.collection("activities");
+
+//single user insert
+    app.post('/users',async(req,res)=>{
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+
+    //multiple user 
+    app.post('/users',async(req,res)=>{
+      const user = req.body
+      const result = await usersCollection.insertMany(user)
+    })
+    //delete user 
+    app.delete('/users/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result =await usersCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    //activities
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log("You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
